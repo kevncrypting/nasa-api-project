aa99ec62d284ad9506213de76c140aeb0aac5bb1
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-26 10:54:48 -0400

    Tests API on render

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+
 function App() {
+  let handleClick = () => {
+    console.log(`Picture was clicked!`);
+  };
+
+  const [imageObject, setImageObject] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/search/photos?client_id=WvDugZQIVEQspCU2bZxy4aVoOpfksRsaUfTlC7d9kt4&query=space&order_by=popular&page=1&per_page=10`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImageObject(data.results);
+        console.log(imageObject);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
+        {imageObject.forEach((image) => {
+          <Card
+            imageUrl={image.urls.small}
+            title={image.description}
+            onPictureClick={() => handleClick()}
+          />;
+        })}
     </>
   );
 }
